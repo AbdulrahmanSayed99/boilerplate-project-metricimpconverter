e41ab368d51b173410b9435e9fb387abb85ee196
AbdulrahmanSayed99 <abdelrhmanelbrolsy@yahoo.com>
2024-10-12 21:11:09 +0300

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -6,5 +6,33 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
+  app.route('/api/convert').get((req,res)=>{
+    let input = req.query.input
+    let initNum = convertHandler.getNum(input);
+    let initUnit= convertHandler.getUnit(input);
+    let returnNum= convertHandler.convert(initNum, initUnit);
+    let returnUnit= convertHandler.getReturnUnit(initUnit);
+    let string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
 
-};
+    if(initNum === 'invalid number' && initUnit === 'invalid unit'){
+     return res.json('invalid number and unit')
+    }
+  
+    if(initNum === 'invalid number'){
+      return res.json('invalid number')
+    }  
+  
+    if(initUnit === 'invalid unit'){
+      return res.json('invalid unit')
+    }  
+  
+    let responseObject = {}
+    responseObject['initNum'] = parseFloat(initNum)
+    responseObject['initUnit'] = initUnit
+    responseObject['returnNum'] = parseFloat(returnNum)
+    responseObject['returnUnit'] = returnUnit
+    responseObject['string'] = string
+  
+    res.json(responseObject)
+  });
+  }
